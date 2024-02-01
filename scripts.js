2465da54ba92dd068192de776e23f0f3e886a787
Rahulwagh07 <rahulwagh3774@gmail.com>
2024-02-02 05:05:48 +0530

    updated & added complete styling

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
- 
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const copy = document.querySelector(".copy");
+const qrfront = document.querySelector(".qr-front");
+const qrback = document.querySelector(".qr-back");
+const qrCodeUrl = './assests/qr.png';
 
 //function to open modal
 const openModal = () => {
@@ -34,27 +35,13 @@ async function copyLink() {
   //function to share bio link on twitter
   function shareOnTwitter() {
     // Construct the tweet text
-    var tweetText = encodeURIComponent("Checkout bio link of Rahul Wagh: https://rahulwagh.bio.link");
+    var tweetText = encodeURIComponent("Checkout this bio link of Rahul Wagh: https://rahulwagh.bio.link");
 
     // Open Twitter in a new window with the share URL
     window.open("https://twitter.com/intent/tweet?text=" + tweetText, "_blank");
   }
   
-
- //function to open gmail with subject, body
-// function shareViaGmail() {
-//     var recipientEmail = '';  
-//     var subject = 'Chekout this Bio Link';
-//     var body = 'Checkout bio link of Rahul Wagh: https://rahulwagh.bio.link';
-
-//     var gmailLink = 'https://mail.google.com/mail/?view=cm&fs=1' +
-//         '&to=' + recipientEmail +
-//         '&su=' + encodeURIComponent(subject) +
-//         '&body=' + encodeURIComponent(body);
-
-//     window.open(gmailLink, '_blank');
-// }
-
+ 
 //function to share via Email
 function shareViaEmail() {
     var recipientEmail = ''; 
@@ -78,3 +65,30 @@ function shareViaWhatsApp() {
 }
 
 
+function showDownloadQR() {
+    qrfront.style.display = 'none';
+    qrback.style.display = 'flex';
+}
+
+function showFrontQR() {
+    qrfront.style.display = 'flex';
+    qrback.style.display = 'none';
+}
+  
+function downloadQRCode() {
+    // create an anchor element
+    var downloadLink = document.createElement('a');
+    downloadLink.href = qrCodeUrl;
+    downloadLink.download = 'qr_code.png';
+
+    // append the created  element to the document body
+    document.body.appendChild(downloadLink);
+
+    // trigger a click event on the anchor element
+    downloadLink.click();
+
+    // Remove the anchor element from the document body
+    document.body.removeChild(downloadLink);
+}
+
+ 
